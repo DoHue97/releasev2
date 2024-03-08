dbf9873a5b2479385dc70c4011fbb40febcf605b
Đỗ Thị Huệ <hue.do@crmi.vn>
2024-03-08 16:50:30 +0700

    first commit

---
+++ static/js/901.6b09629c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkconsumerportal_v2=self.webpackChunkconsumerportal_v2||[]).push([[901],{26901:function(e,u,s){s.r(u);u.default={}}}]);
+//# sourceMappingURL=901.6b09629c.chunk.js.map
---
+++ static/js/967.a7afb6b6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkconsumerportal_v2=self.webpackChunkconsumerportal_v2||[]).push([[967],{68967:function(e,u,s){s.r(u);u.default={}}}]);
+//# sourceMappingURL=967.a7afb6b6.chunk.js.map
